29b21e6e392094579152409e9ce76c54f148ad46
Aadityyaaaa-0410 <aadityasingh2108@gmail.com>
2025-10-28 13:09:00 +0530

    Update API_BASE URL for todo fetching

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -61,7 +61,7 @@ function TodoList({ todos, deleteTodo }) {
 function App() {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
-  const API_BASE = "http://ff-1740939327.us-east-1.elb.amazonaws.cm";
+  const API_BASE = "http://alb-1122585112.us-east-1.elb.amazonaws.com";
 
   // ✅ Fetch all todos
   const fetchTodos = async () => {
